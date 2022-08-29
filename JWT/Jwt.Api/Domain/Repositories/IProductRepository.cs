d27cc87dad2ddae108652f04197d3832bceaa629
Ahmet Aydeniz <mcrear.master@gmail.com>
2022-08-29 17:36:31 +0300

    Product repository and crud methods created

---
+++ JWT/Jwt.Api/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Jwt.Api.Domain.Model;
+
+namespace Jwt.Api.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> ListAsync();
+        Task AddProduct(Product product);
+        Task UpdateProduct(Product product);
+        Task<Product> GetProductyById(int id);
+        Task DeleteProductAsync(int id);
+    }
+}
---
+++ JWT/Jwt.Api/Domain/Repositories/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Jwt.Api.Domain.Data;
+using Jwt.Api.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jwt.Api.Domain.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly JwtDbContext jwtDbContext;
+
+        public ProductRepository(JwtDbContext jwtDbContext)
+        {
+            this.jwtDbContext = jwtDbContext;
+        }
+
+        public async Task AddProduct(Product product)
+        {
+            await jwtDbContext.Products.AddAsync(product);
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var item = await GetProductyById(id);
+
+            jwtDbContext.Products.Remove(item);
+        }
+
+        public async Task<Product> GetProductyById(int id)
+        {
+            return await jwtDbContext.Products.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> ListAsync()
+        {
+            return await jwtDbContext.Products.ToListAsync();
+        }
+
+        public async Task UpdateProduct(Product product)
+        {
+            jwtDbContext.Products.Update(product);
+        }
+    }
+}
