feb1281b695c87f6fa4e157075f60899f89a154b
Ahmet Aydeniz <mcrear.master@gmail.com>
2022-08-29 19:05:18 +0300

    Product resource and mapper added

---
+++ JWT/Jwt.Api/Mapping/ProductMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Jwt.Api.Domain.Model;
+using Jwt.Api.Resources;
+
+namespace Jwt.Api.Mapping
+{
+    public class ProductMapper : Profile
+    {
+        public ProductMapper()
+        {
+            CreateMap<ProductResource, Product>();
+            CreateMap<Product, ProductResource>();
+        }
+    }
+}
---
+++ JWT/Jwt.Api/Resources/ProductResource.cs
@@ -0,0 +1,9 @@
+﻿namespace Jwt.Api.Resources
+{
+    public class ProductResource
+    {
+        public int Name { get; set; }
+        public string Category { get; set; }
+        public string Price { get; set; }
+    }
+}
