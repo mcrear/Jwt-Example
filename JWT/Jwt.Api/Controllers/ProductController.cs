686eb3b1d1eb99a4a4046f3e148cd344d00c3361
Ahmet Aydeniz <mcrear.master@gmail.com>
2022-08-29 21:59:00 +0300

    Product controller crud operation added

---
+++ JWT/Jwt.Api/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using Jwt.Api.Domain.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Jwt.Api.Resources;
+using Jwt.Api.Extensions;
+using Jwt.Api.Domain.Model;
+
+namespace Jwt.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            var res = await _productService.ListAsync();
+            if (res.Success)
+                return Ok(res.Data);
+            return BadRequest(res.Message);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var res = await _productService.GetAsync(id);
+            if (res.Success)
+                return Ok(res.Data);
+            return BadRequest(res.Message);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductResource product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.GetErrorMessages());
+            }
+            var prod = _mapper.Map<ProductResource, Product>(product);
+
+            var res = await _productService.AddProduct(prod);
+
+            if (res.Success)
+                return Ok(res.Data);
+            return BadRequest(res.Message);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var res = await _productService.DeleteAsync(id);
+
+            if (res.Success)
+                return Ok();
+            return BadRequest(res.Message);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductResource product, int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.GetErrorMessages());
+            }
+
+            var prod = _mapper.Map<ProductResource, Product>(product);
+
+            var res = await _productService.UpdateAsync(prod, id);
+
+            if (res.Success)
+                return Ok(res.Data);
+            return BadRequest(res.Message);
+        }
+    }
+}
--- JWT/Jwt.Api/Resources/ProductResource.cs
+++ JWT/Jwt.Api/Resources/ProductResource.cs
@@ -1,9 +1,14 @@
-﻿namespace Jwt.Api.Resources
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jwt.Api.Resources
 {
     public class ProductResource
     {
+        [Required]
         public int Name { get; set; }
+        [Required]
         public string Category { get; set; }
-        public string Price { get; set; }
+        [Required]
+        public decimal? Price { get; set; }
     }
 }
