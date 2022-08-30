342d18bec0c7db4ed38c974b9e1bc044df141ad2
Ahmet Aydeniz <mcrear.master@gmail.com>
2022-08-30 21:19:34 +0300

    Test spa added

---
+++ JWT/Jwt.Spa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jwt.Spa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
