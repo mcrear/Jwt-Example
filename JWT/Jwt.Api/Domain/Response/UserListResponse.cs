c6381535c65d516fdd81a36130324adde05a1e1c
Ahmet Aydeniz <mcrear.master@gmail.com>
2022-08-30 10:20:05 +0300

    User response added

---
+++ JWT/Jwt.Api/Domain/Response/UserListResponse.cs
@@ -0,0 +1,16 @@
+﻿using Jwt.Api.Domain.Model;
+
+namespace Jwt.Api.Domain.Response
+{
+    public class UserListResponse:BaseResponse<IEnumerable<User>>
+    {
+        public UserListResponse(IEnumerable<User> user) : base(true, "", user)
+        {
+
+        }
+        public UserListResponse(string message) : base(false, message, null)
+        {
+
+        }
+    }
+}
---
+++ JWT/Jwt.Api/Domain/Response/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿using Jwt.Api.Domain.Model;
+
+namespace Jwt.Api.Domain.Response
+{
+    public class UserResponse:BaseResponse<User>
+    {
+        public UserResponse(User user) : base(true, "", user)
+        {
+
+        }
+        public UserResponse(string message) : base(false, message, null)
+        {
+
+        }
+    }
+}
